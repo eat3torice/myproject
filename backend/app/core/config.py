9accaaccb3aef8f534b1fd46eb6ab4c9cdfee31e
eat3torice <khanhpham422004@gmail.com>
2025-12-01 14:41:44 +0700

    Set default value for ALGORITHM to HS256

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str = Field(..., env="DATABASE_URL")
     SECRET_KEY: str = Field(..., env="SECRET_KEY")
-    ALGORITHM: str = Field(..., env="ALGORITHM")
+    ALGORITHM: str = Field("HS256", env="ALGORITHM")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(30, env="ACCESS_TOKEN_EXPIRE_MINUTES")
 
     class Config:
